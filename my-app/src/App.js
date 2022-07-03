e95cf917b048917a01dbf1a1f49ab85ae3b360b5
afro-juliano <afro.juliano@gmail.com>
2022-07-03 19:27:00 -0300

    counter-done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+/*import logo from './logo.svg';*/
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class Counter extends Component {
+  state = {
+    count: 42
+  };
+
+  handleClick = () => {
+    this.setState(({ count }) => ({
+      count: count + 1
+    }));
+  };
+
+  render() {
+    return (
+      <>
+       <div>
+            <h2>{this.state.count}</h2>
+       </div>
+       <style>{`
+          .counter-button {
+            font-size: 1rem;
+            padding: 5px 10px;
+            color: #585858;
+        }
+      `}</style>
+      <button onClick={this.handleClick}>Click</button>
+      </>
+    );
+  }
 }
-
-export default App;
